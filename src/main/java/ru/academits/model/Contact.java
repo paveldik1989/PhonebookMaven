e10dbcd6a04ee49884380aa6c65c89346cf732a9
paveldik1989 <paveldik1989@gmail.com>
2023-04-21 21:33:19 +0700

    Initial commit

---
+++ src/main/java/ru/academits/model/Contact.java
@@ -0,0 +1,66 @@
+package ru.academits.model;
+
+import java.util.Objects;
+
+public class Contact {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private boolean important;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isImportant() {
+        return important;
+    }
+
+    public void setImportant(boolean important) {
+        this.important = important;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return  firstName.equals(contact.firstName) && lastName.equals(contact.lastName) && phone.equals(contact.phone);
+    }
+
+    //id == contact.id && important == contact.important &&
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, phone, important);
+    }
+}
